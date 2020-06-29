7ef5a9fb6d9d2260417717f70721a8d26a5de6bc
Hinrich Mahler <hinrich.mahler@freenet.de>
2020-06-29 22:32:20 +0200

    More registration tests

--- bot/registration.py
+++ bot/registration.py
@@ -9,16 +9,16 @@ from components import Member
 
 ACCEPT_REGISTRATION = 'accept_registration {} {}'
 """:obj:`str`: Callback data for accepting a registration request with a suggested members data.
-Use as ``ACCEPT_REGISTRATION_MEMBER.format(number)``. Use ``''`` for number, if no given data is
-to be accepted."""
+Use as ``ACCEPT_REGISTRATION_MEMBER.format(chat_id, data_index)``. Use ``''`` for data_index, if no
+given data is to be accepted."""
 ACCEPT_REGISTRATION_PATTERN = r'accept_registration (\d*)(?: (\d*))?'
 """:obj:`str`: Callback data  pattern for accepting a registration request with a suggested members
 data. ``context.match.group(1)`` will be users id and ``context.match.group(2) the index of the
 accepted member date or :obj:`None`."""
 DENY_REGISTRATION = 'deny_registration {}'
 """:obj:`str`: Callback data for denying a registration request.
-Use as ``DENY_REGISTRATION.format(number)``."""
-DENY_REGISTRATION_PATTERN = r'accept_registration (\d*)'
+Use as ``DENY_REGISTRATION.format(chat_id)``."""
+DENY_REGISTRATION_PATTERN = r'deny_registration (\d*)'
 """:obj:`str`: Callback data  pattern for accepting a registration request with a suggested members
 data. ``context.match.group(1)`` will be users id."""
 
@@ -48,12 +48,17 @@ def check_registration_status(update: Update, context: CallbackContext) -> None:
     if not update.effective_user:
         raise DispatcherHandlerStop()
     user_id = update.effective_user.id
+    bot_data = context.bot_data
 
     # User is registered
-    if user_id in context.bot_data[ORCHESTRA_KEY].members:
+    if user_id in bot_data[ORCHESTRA_KEY].members:
         return
 
-    if user_id in context.bot_data[PENDING_REGISTRATIONS_KEY]:
+    # Admins can do anything
+    if user_id == bot_data[ADMIN_KEY]:
+        return
+
+    if user_id in bot_data[PENDING_REGISTRATIONS_KEY]:
         if update.effective_message:
             update.effective_message.reply_text(
                 'Du wirst benachrichtigt, sobald Deine Anmeldungs-Anfrage bearbeitet wurde. 📬 '
@@ -63,7 +68,7 @@ def check_registration_status(update: Update, context: CallbackContext) -> None:
                 reply_markup=DOCS_KEYBOARD)
         raise DispatcherHandlerStop()
 
-    if user_id in context.bot_data[DENIED_USERS_KEY]:
+    if user_id in bot_data[DENIED_USERS_KEY]:
         raise DispatcherHandlerStop()
 
     if update.message:
@@ -150,31 +155,36 @@ def accept_registration_request(update: Update, context: CallbackContext) -> Non
         context: The context as provided by the :class:`telegram.ext.Dispatcher`.
     """
     bot_data = context.bot_data
-    user_id = context.match.group(1)
+    user_id = int(context.match.group(1))
     user = context.bot.get_chat_member(user_id, user_id).user
     profile_photos = user.get_profile_photos()
-    profile_ile_id = profile_photos[0][-1].file_id if profile_photos else None
+
+    if profile_photos.total_count >= 1:
+        profile_ile_id = profile_photos.photos[0][-1].file_id
+    else:
+        profile_ile_id = None
 
     idx = context.match.group(2)
-    if idx is None:
+    if not idx:
         new_member = Member(user_id,
                             first_name=user.first_name,
                             last_name=user.last_name,
                             photo_file_id=profile_ile_id)
     else:
+        idx = int(idx)
         new_member = bot_data[PENDING_REGISTRATIONS_KEY][user_id][idx]
         new_member.photo_file_id = profile_ile_id
 
     bot_data[ORCHESTRA_KEY].register_member(new_member)
-    del bot_data[PENDING_REGISTRATIONS_KEY][user_id]
+    bot_data[PENDING_REGISTRATIONS_KEY].pop(user_id, None)
 
-    text = f'Du bis jetzt mit den folgenden Daten angemeldet:\n\n{new_member.to_str()}\n\n.'
+    text = f'Du bis jetzt mit den folgenden Daten angemeldet:\n\n{new_member.to_str()}\n\n'
     if profile_ile_id:
         text += 'Als Photo wurde Dein Telegram-Profilbild gesetzt.'
     text += 'Um die Daten zu bearbeiten, sende den Befehl /daten_bearbeiten .'
 
     context.bot.send_message(chat_id=user_id, text=text)
-    update.effective_message.reply_text('Nutzer erfolgreich angemeldet.')
+    update.effective_message.edit_text('Nutzer erfolgreich angemeldet.')
 
 
 def deny_registration_request(update: Update, context: CallbackContext) -> None:
@@ -186,14 +196,14 @@ def deny_registration_request(update: Update, context: CallbackContext) -> None:
         context: The context as provided by the :class:`telegram.ext.Dispatcher`.
     """
     bot_data = context.bot_data
-    user_id = context.match.group(1)
-    del bot_data[PENDING_REGISTRATIONS_KEY][user_id]
+    user_id = int(context.match.group(1))
+    bot_data[PENDING_REGISTRATIONS_KEY].pop(user_id, None)
     bot_data[DENIED_USERS_KEY].append(user_id)
 
     context.bot.send_message(chat_id=user_id,
                              text='Deine Anfrage wurde abgelehnt. Ich werde von jetzt an nicht '
                              'mehr auf Dich reagieren.')
-    update.effective_message.reply_text('Nutzer wurde abgelehnt und wird jetzt ignoriert.')
+    update.effective_message.edit_text('Nutzer wurde abgelehnt und wird jetzt ignoriert.')
 
 
 ACCEPT_REGISTRATION_HANDLER = CallbackQueryHandler(accept_registration_request,
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -7,8 +7,10 @@ from geopy import Photon
 from telegram import Update, Message, User, Chat, CallbackQuery, MessageEntity
 from telegram.ext import TypeHandler
 
-from bot import ORCHESTRA_KEY, PENDING_REGISTRATIONS_KEY, DENIED_USERS_KEY, REGISTRATION_PATTERN
+from bot import (ORCHESTRA_KEY, PENDING_REGISTRATIONS_KEY, DENIED_USERS_KEY, REGISTRATION_PATTERN,
+                 ADMIN_KEY)
 from bot.setup import register_dispatcher
+from bot.registration import ACCEPT_REGISTRATION, DENY_REGISTRATION
 from components import Member, Orchestra
 from tests.addresses import get_address_from_cache
 from tests.check_file_path import check_file_path
@@ -41,6 +43,7 @@ class TestRegistration:
         assert self.handler_executed
 
     def test_check_registration_status_pending_registration(self, dp, monkeypatch, chat_id):
+        dp.bot_data[ADMIN_KEY] = None
         orig_send_message = dp.bot.send_message
         answer_message = None
 
@@ -64,12 +67,14 @@ class TestRegistration:
         assert not self.handler_executed
 
     def test_check_registration_status_denied_user(self, dp):
+        dp.bot_data[ADMIN_KEY] = None
         dp.bot_data[DENIED_USERS_KEY].append(123)
         update = Update(123, message=Message(123, User(123, 'user', False), None, None))
         dp.process_update(update)
         assert not self.handler_executed
 
     def test_check_registration_status_not_registered(self, dp, chat_id, monkeypatch):
+        dp.bot_data[ADMIN_KEY] = None
         orig_send_message = dp.bot.send_message
         answer_message = None
 
@@ -204,3 +209,123 @@ class TestRegistration:
         dp.process_update(update)
         assert 'kein passendes Mitglied' in messages[-1].text
         assert dp.bot_data[PENDING_REGISTRATIONS_KEY][chat_id] == []
+
+    def test_accept_registration_request_no_suggestion(self, dp, bot, chat_id, monkeypatch):
+        message = bot.send_message(chat_id, 'test_message')
+        dp.bot_data[PENDING_REGISTRATIONS_KEY][chat_id] = []
+
+        orig_send_message = bot.send_message
+        orig_edit_message = bot.edit_message_text
+        reply_message = None
+
+        def send_message(*args, **kwargs):
+            nonlocal reply_message
+            reply_message = orig_send_message(*args, **kwargs)
+            return reply_message
+
+        def edit_message_text(*args, **kwargs):
+            nonlocal message
+            message = orig_edit_message(*args, **kwargs)
+            return message
+
+        monkeypatch.setattr(bot, 'send_message', send_message)
+        monkeypatch.setattr(bot, 'edit_message_text', edit_message_text)
+
+        update = Update(123,
+                        callback_query=CallbackQuery(1,
+                                                     User(chat_id,
+                                                          is_bot=False,
+                                                          first_name='XXXXXX'),
+                                                     Chat(chat_id, Chat.PRIVATE),
+                                                     message=message,
+                                                     data=ACCEPT_REGISTRATION.format(chat_id, '')))
+
+        dp.process_update(update)
+
+        chat_member = bot.get_chat_member(chat_id, chat_id).user
+        member = dp.bot_data[ORCHESTRA_KEY].members[chat_id]
+        assert member.first_name == chat_member.first_name
+        assert member.last_name == chat_member.last_name
+        assert 'mit den folgenden Daten angemeldet' in reply_message.text
+        assert 'erfolgreich angemeldet' in message.text
+        assert chat_id not in dp.bot_data[PENDING_REGISTRATIONS_KEY]
+
+    def test_accept_registration_request_with_suggestion(self, dp, bot, chat_id, monkeypatch):
+        message = bot.send_message(chat_id, 'test_message')
+        dp.bot_data[PENDING_REGISTRATIONS_KEY][chat_id] = [
+            Member(chat_id, first_name='0', last_name='0'),
+            Member(chat_id, first_name='1', last_name='1')
+        ]
+
+        orig_send_message = bot.send_message
+        orig_edit_message = bot.edit_message_text
+        reply_message = None
+
+        def send_message(*args, **kwargs):
+            nonlocal reply_message
+            reply_message = orig_send_message(*args, **kwargs)
+            return reply_message
+
+        def edit_message_text(*args, **kwargs):
+            nonlocal message
+            message = orig_edit_message(*args, **kwargs)
+            return message
+
+        monkeypatch.setattr(bot, 'send_message', send_message)
+        monkeypatch.setattr(bot, 'edit_message_text', edit_message_text)
+
+        update = Update(123,
+                        callback_query=CallbackQuery(1,
+                                                     User(chat_id,
+                                                          is_bot=False,
+                                                          first_name='XXXXXX'),
+                                                     Chat(chat_id, Chat.PRIVATE),
+                                                     message=message,
+                                                     data=ACCEPT_REGISTRATION.format(chat_id, 1)))
+
+        dp.process_update(update)
+
+        member = dp.bot_data[ORCHESTRA_KEY].members[chat_id]
+        assert member.first_name == '1'
+        assert member.last_name == '1'
+        assert 'mit den folgenden Daten angemeldet' in reply_message.text
+        assert 'erfolgreich angemeldet' in message.text
+        assert chat_id not in dp.bot_data[PENDING_REGISTRATIONS_KEY]
+
+    def test_deny_registration_request(self, dp, bot, chat_id, monkeypatch):
+        message = bot.send_message(chat_id, 'test_message')
+        dp.bot_data[PENDING_REGISTRATIONS_KEY][chat_id] = []
+
+        orig_send_message = bot.send_message
+        orig_edit_message = bot.edit_message_text
+        reply_message = None
+
+        def send_message(*args, **kwargs):
+            nonlocal reply_message
+            reply_message = orig_send_message(*args, **kwargs)
+            return reply_message
+
+        def edit_message_text(*args, **kwargs):
+            nonlocal message
+            message = orig_edit_message(*args, **kwargs)
+            return message
+
+        monkeypatch.setattr(bot, 'send_message', send_message)
+        monkeypatch.setattr(bot, 'edit_message_text', edit_message_text)
+
+        update = Update(123,
+                        callback_query=CallbackQuery(1,
+                                                     User(chat_id,
+                                                          is_bot=False,
+                                                          first_name='XXXXXX'),
+                                                     Chat(chat_id, Chat.PRIVATE),
+                                                     message=message,
+                                                     data=DENY_REGISTRATION.format(chat_id)))
+
+        dp.process_update(update)
+
+        assert chat_id not in dp.bot_data[ORCHESTRA_KEY].members
+        assert 'Anfrage wurde abgelehnt' in reply_message.text
+        assert 'wird jetzt ignoriert' in message.text
+        assert chat_id in dp.bot_data[DENIED_USERS_KEY]
+        assert chat_id not in dp.bot_data[PENDING_REGISTRATIONS_KEY]
